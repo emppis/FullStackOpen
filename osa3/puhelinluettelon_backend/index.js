d4ad627ecba65a7af999af86e399c50743e16afa
Emmiina Kivelä <kivela.emmiina@gmail.com>
2025-07-22 20:35:20 +0300

    3.1-3.6

---
+++ osa3/puhelinluettelon_backend/index.js
@@ -0,0 +1,103 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  { 
+      "name": "Arto Hellas", 
+      "number": "040-123456",
+      "id": "1"
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523",
+      "id": "2"
+    },
+    { 
+      "name": "Dan Abramov", 
+      "number": "12-43-234345",
+      "id": "3"
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122",
+      "id": "4"
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  let id
+  do {
+    id = Math.floor(Math.random() * 1000000)
+  } while (persons.some(p => p.id === String(id)))
+  return String(id)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log('POST request received', body)
+
+  if (!body.name || !body.number) {
+    console.error('Error: name or number missing')
+    return response.status(400).json({ error: 'name or number is missing'})
+  }
+
+  const nameExists = persons.some(person => person.name === body.name)
+  if (nameExists) {
+    console.error(`Error: name "${body.name} already exists`)
+    return response.status(400).json({ error: 'name must be unique' })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+  console.log(`Added new person: ${person.name} (${person.number}), ID: ${person.id}`)
+  response.json(person)
+})
+
+
+app.get('/info', (request, response) => {
+  const numberOfPersons = persons.length
+  const currentTime = new Date()
+
+  response.send(`
+    <p>Phonebook has info for ${numberOfPersons} people</p>
+    <p>${currentTime}</p>
+  `)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
