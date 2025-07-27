b06383995b78cf8b63c7668dc9d97e2f4f82b697
Emmiina Kivelä <kivela.emmiina@gmail.com>
2025-07-27 18:24:52 +0300

    uibuild

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
-app.use(cors())
+
 app.use(express.json())
 app.use(express.static('dist'))
 morgan.token('body', (req) => JSON.stringify(req.body))
